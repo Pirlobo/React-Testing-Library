1351267508b75b0baa76c43b20f6cd778aac71b7
Anh Nguyen <anh.t.nguyen07@sjsu.edu>
2022-06-13 10:01:59 -0700

    adding test cases

--- src/services/CommentService.js
+++ src/services/CommentService.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 const POST_API_BASE_URL = "https://jsonplaceholder.typicode.com/posts/";
 
 class CommentService {
-
     getCommentsByPostId(postId) {
         return axios.get(POST_API_BASE_URL +  postId + '/comments');
     }
